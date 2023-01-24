10276c7b1545a0a8776d72386a84abe14c057d6a
NadinRassi <sofianadinrassi@gmail.com>
2023-01-24 12:47:58 -0300

    2 webs apis creadas

---
+++ WebApiPubs/Controllers/PublisherController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiPubs.Models;
+using Publisher = WebApiPubs.Models.Publishers;
+
+namespace WebApiPubs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController : ControllerBase
+    {
+        private pubsContext context;
+
+        public PublisherController(pubsContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Publisher>> Get()
+        {
+            return context.Publishers.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Publisher> GetById(string id)
+        {
+            Publisher publisher = (from p in context.Publishers
+                                   where p.PubId == id
+                                   select p).SingleOrDefault();
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            return publisher;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Publisher publisher)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            context.Publishers.Add(publisher);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Publisher publisher)
+        {
+            if (publisher.PubId != id)
+            {
+                return BadRequest();
+            }
+            context.Entry(publisher).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Publisher> Delete(string id)
+        {
+            Publisher publisher = (from p in context.Publishers
+                                   where p.PubId == id
+                                   select p).SingleOrDefault();
+
+            if (publisher == null)
+            {
+                return NotFound();
+            }
+            context.Publishers.Remove(publisher);
+            context.SaveChanges();
+            return publisher;
+        }
+    }
+}
